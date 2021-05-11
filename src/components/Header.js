ec2c3a27b7e24529acf7aa9b6ae7f571051c686e
Leon Brown <leon.brown71@gmail.com>
2021-05-11 13:50:54 +0100

    layout header and hero and create styles

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">leonbrown.dev</div>
+      <div className="links">
+        <span>about</span>
+        <span>skills</span>
+        <span>work</span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
