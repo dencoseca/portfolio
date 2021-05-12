f6431f58b7936fd3bb43c005d51c239039150368
Leon Brown <leon.brown71@gmail.com>
2021-05-12 15:30:00 +0100

    create and style hero text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,61 +1,30 @@
 import React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
 
 function Hero() {
   return (
-    <section className="hero" data-scroll-section>
-      <div className="container">
-        <div className="text">
-          <h1 data-scroll>
-            <span data-scroll data-scroll-speed="-2" data-scroll-position="top">
-              H
-            </span>
-            <span data-scroll data-scroll-speed="-1.9" data-scroll-position="top">
-              i
-            </span>
-            <span
-              data-scroll
-              data-scroll-speed="-1.8"
-              data-scroll-position="top"
-            >
-              ,
-            </span>{' '}
-            <span data-scroll data-scroll-speed="-1.7" data-scroll-position="top">
-              I
-            </span>
-            <span data-scroll data-scroll-speed="-1.6" data-scroll-position="top">
-              '
-            </span>
-            <span
-              data-scroll
-              data-scroll-speed="-1.5"
-              data-scroll-position="top"
-            >
-              m
-            </span>{' '}
-            <span data-scroll data-scroll-speed="-1.4" data-scroll-position="top">
-              L
-            </span>
-            <span data-scroll data-scroll-speed="-1.3" data-scroll-position="top">
-              e
-            </span>
-            <span data-scroll data-scroll-speed="-1.2" data-scroll-position="top">
-              o
-            </span>
-            <span data-scroll data-scroll-speed="-1" data-scroll-position="top">
-              n
-            </span>
-          </h1>
-          <p className="tagline">A developer based in Sheffield, UK.</p>
+    <div className="container">
+      <section className="hero" data-scroll-section>
+        <div id="sticky-target" data-scroll-offset="0, 20%">
+          <div
+            className="sentence sentence--start"
+            data-scroll
+            data-scroll-sticky
+            data-scroll-target="#sticky-target"
+          >
+            Hi, I'm
+          </div>
+          <div className="sentence sentence--end">
+            <div className="sentence--end__line">a developer</div>
+            <div className="sentence--end__line">based in Sheffield, UK</div>
+            <div className="sentence--end__line">
+              looking for my first professional position
+            </div>
+            <div className="sentence--end__line">self-taught</div>
+            <div className="sentence--end__line">hard-working</div>
+          </div>
         </div>
-      </div>
-      <StaticImage
-        className="image"
-        src="https://images.unsplash.com/photo-1519219788971-8d9797e0928e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2528&q=80"
-        alt="desk"
-        layout="fullWidth"
-      />
-    </section>
+      </section>
+    </div>
   )
 }
 
