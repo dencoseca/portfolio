156b85f4ccdec10b7b74cd0cbcd21a5cf73b04ff
Leon Brown <leon.brown71@gmail.com>
2021-05-14 19:07:04 +0100

    animate hero text with locomotive scroll

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 
 // Animations
@@ -31,6 +31,16 @@ const heroLine = {
 }
 
 function Hero() {
+  useEffect(() => {
+    const lines = document.querySelectorAll('.hero__inner .line')
+
+    setTimeout(() => {
+      lines.forEach(line => {
+        line.style.overflow = 'visible'
+      })
+    }, 1900)
+  }, [])
+
   return (
     <AnimatePresence>
       <section className="hero" id="hero" data-scroll-section>
@@ -43,17 +53,254 @@ function Hero() {
         >
           <div className="line">
             <motion.div className="title first" variants={heroLine}>
-              Hi, I'm Leon,
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="1"
+                data-scroll-delay="0.05"
+              >
+                H
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="2"
+                data-scroll-delay="0.05"
+              >
+                i,
+              </span>{' '}
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="4"
+                data-scroll-delay="0.05"
+              >
+                I'
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="3"
+                data-scroll-delay="0.05"
+              >
+                m
+              </span>{' '}
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="1"
+                data-scroll-delay="0.05"
+              >
+                L
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="2"
+                data-scroll-delay="0.05"
+              >
+                e
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="3"
+                data-scroll-delay="0.05"
+              >
+                o
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="1"
+                data-scroll-delay="0.05"
+              >
+                n,
+              </span>
             </motion.div>
           </div>
           <div className="line">
             <motion.div className="title second" variants={heroLine}>
-              a developer based
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="2"
+                data-scroll-delay="0.05"
+              >
+                a
+              </span>{' '}
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="1"
+                data-scroll-delay="0.05"
+              >
+                d
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="3"
+                data-scroll-delay="0.05"
+              >
+                e
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="4"
+                data-scroll-delay="0.05"
+              >
+                v
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="2"
+                data-scroll-delay="0.05"
+              >
+                e
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="3"
+                data-scroll-delay="0.05"
+              >
+                l
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="1"
+                data-scroll-delay="0.05"
+              >
+                o
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="2"
+                data-scroll-delay="0.05"
+              >
+                p
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="4"
+                data-scroll-delay="0.05"
+              >
+                e
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="1"
+                data-scroll-delay="0.05"
+              >
+                r
+              </span>{' '}
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="3"
+                data-scroll-delay="0.05"
+              >
+                b
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="2"
+                data-scroll-delay="0.05"
+              >
+                a
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="3"
+                data-scroll-delay="0.05"
+              >
+                s
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="1"
+                data-scroll-delay="0.05"
+              >
+                e
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="4"
+                data-scroll-delay="0.05"
+              >
+                d
+              </span>
             </motion.div>
           </div>
           <div className="line">
             <motion.div className="title third" variants={heroLine}>
-              in the UK.
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="3"
+                data-scroll-delay="0.05"
+              >
+                i
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="1"
+                data-scroll-delay="0.05"
+              >
+                n
+              </span>{' '}
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="2"
+                data-scroll-delay="0.05"
+              >
+                t
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="1"
+                data-scroll-delay="0.05"
+              >
+                h
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="4"
+                data-scroll-delay="0.05"
+              >
+                e
+              </span>{' '}
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="3"
+                data-scroll-delay="0.05"
+              >
+                u
+              </span>
+              <span
+                data-scroll
+                data-scroll-position="top"
+                data-scroll-speed="2"
+                data-scroll-delay="0.05"
+              >
+                k.
+              </span>
             </motion.div>
           </div>
         </motion.div>
