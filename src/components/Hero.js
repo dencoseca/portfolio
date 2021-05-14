99607b1ec7568159241f7dd4dd9fde39d77dde8e
Leon Brown <leon.brown71@gmail.com>
2021-05-14 13:20:35 +0100

    change hero title, add General Sans and Schnyder Cond M Light Fonts

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,79 +7,16 @@ const transition = {
   ease: [0, 0.55, 0.45, 1],
 }
 
-const sentenceStart = {
-  initial: {
-    opacity: 0,
-    x: -100,
-  },
-  animate: {
-    opacity: 1,
-    x: 0,
-    transition: { ...transition },
-  },
-}
-
-const sentenceEnd = {
-  animate: {
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
-}
-
-const line = {
-  initial: {
-    opacity: 0,
-    x: 100,
-  },
-  animate: {
-    opacity: 1,
-    x: 0,
-    transition: { ...transition },
-  },
-}
-
 function Hero() {
   return (
     <AnimatePresence>
       <section className="hero" id="hero" data-scroll-section>
-        <div className="hero__inner" id="sticky-target">
-          <motion.div
-            className="sentence sentence--start"
-            data-scroll
-            data-scroll-sticky
-            data-scroll-target="#sticky-target"
-            initial="initial"
-            animate="animate"
-            variants={sentenceStart}
-          >
-            Hi, I'm
-          </motion.div>
-          <motion.div
-            className="sentence sentence--end"
-            initial="initial"
-            animate="animate"
-            variants={sentenceEnd}
-          >
-            <motion.div className="line" variants={line}>
-              Leon
-            </motion.div>
-            <motion.div className="line" variants={line}>
-              a developer
-            </motion.div>
-            <motion.div className="line" variants={line}>
-              based in Sheffield, UK
-            </motion.div>
-            <motion.div className="line" variants={line}>
-              looking for my first position
-            </motion.div>
-            <motion.div className="line" variants={line}>
-              self-taught
-            </motion.div>
-            <motion.div className="line" variants={line}>
-              hard-working
-            </motion.div>
-          </motion.div>
+        <div className="container">
+          <div className="hero__inner" id="sticky-target">
+            <motion.div className="title first">Hi, I'm Leon,</motion.div>
+            <motion.div className="title second">a developer based</motion.div>
+            <motion.div className="title third">in the UK.</motion.div>
+          </div>
         </div>
       </section>
     </AnimatePresence>
